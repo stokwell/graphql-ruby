f978e2f399a8af67a6fbd8a9ad2666fae19c0f03
Vitaly Lyapunov <stokwell1@gmail.com>
2017-10-02 17:13:27 +0200

    Authenticating requests

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -5,4 +5,6 @@ Types::LinkType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :url, !types.String
   field :description, !types.String
+
+  field :postedBy, -> { Types::UserType }, property: :user
 end
